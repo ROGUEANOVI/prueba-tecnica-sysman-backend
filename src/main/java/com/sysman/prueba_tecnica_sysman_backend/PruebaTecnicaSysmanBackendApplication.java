3994b3a69cff8e3555d695e73d5045170b7f48a8
rogueanovi <ovidioromero66@gmail.com>
2025-06-28 16:27:41 -0500

    first commit

---
+++ src/main/java/com/sysman/prueba_tecnica_sysman_backend/PruebaTecnicaSysmanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sysman.prueba_tecnica_sysman_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaSysmanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaSysmanBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sysman/prueba_tecnica_sysman_backend/PruebaTecnicaSysmanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysman.prueba_tecnica_sysman_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaSysmanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
